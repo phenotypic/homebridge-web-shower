92e1446d397e09838c81cbf09373076fd79b3a49
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-07-20 00:19:10 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,156 @@
+var Service, Characteristic
+const packageJson = require('./package.json')
+const request = require('request')
+const ip = require('ip')
+const http = require('http')
+
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-web-shower', 'WebShower', WebShower)
+}
+
+function WebShower (log, config) {
+  this.log = log
+
+  this.name = config.name
+  this.apiroute = config.apiroute
+  this.pollInterval = config.pollInterval || 300
+
+  this.listener = config.listener || false
+  this.port = config.port || 2000
+  this.requestArray = ['state']
+
+  this.manufacturer = config.manufacturer || packageJson.author.name
+  this.serial = config.serial || this.apiroute
+  this.model = config.model || packageJson.name
+  this.firmware = config.firmware || packageJson.version
+
+  this.username = config.username || null
+  this.password = config.password || null
+  this.timeout = config.timeout || 3000
+  this.http_method = config.http_method || 'GET'
+
+  if (this.username != null && this.password != null) {
+    this.auth = {
+      user: this.username,
+      pass: this.password
+    }
+  }
+
+  if (this.listener) {
+    this.server = http.createServer(function (request, response) {
+      var parts = request.url.split('/')
+      var partOne = parts[parts.length - 2]
+      var partTwo = parts[parts.length - 1]
+      if (parts.length === 3 && this.requestArray.includes(partOne)) {
+        this.log('Handling request: %s', request.url)
+        response.end('Handling request')
+        this._httpHandler(partOne, partTwo)
+      } else {
+        this.log.warn('Invalid request: %s', request.url)
+        response.end('Invalid request')
+      }
+    }.bind(this))
+
+    this.server.listen(this.port, function () {
+      this.log('Listen server: http://%s:%s', ip.address(), this.port)
+    }.bind(this))
+  }
+
+  this.service = new Service.Valve(this.name)
+}
+
+WebShower.prototype = {
+
+  identify: function (callback) {
+    this.log('Identify requested!')
+    callback()
+  },
+
+  _httpRequest: function (url, body, method, callback) {
+    request({
+      url: url,
+      body: body,
+      method: this.http_method,
+      timeout: this.timeout,
+      rejectUnauthorized: false,
+      auth: this.auth
+    },
+    function (error, response, body) {
+      callback(error, response, body)
+    })
+  },
+
+  _getStatus: function (callback) {
+    var url = this.apiroute + '/status'
+    this.log.debug('Getting status: %s', url)
+
+    this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error getting status: %s', error.message)
+        this.service.getCharacteristic(Characteristic.Active).updateValue(new Error('Polling failed'))
+        callback(error)
+      } else {
+        this.log.debug('Device response: %s', responseBody)
+        var json = JSON.parse(responseBody)
+        this.service.getCharacteristic(Characteristic.Active).updateValue(json.currentState)
+        this.service.getCharacteristic(Characteristic.InUse).updateValue(json.currentState)
+        this.log('Updated state to: %s', json.currentState)
+        callback()
+      }
+    }.bind(this))
+  },
+
+  _httpHandler: function (characteristic, value) {
+    switch (characteristic) {
+      case 'state':
+        this.service.getCharacteristic(Characteristic.InUse).updateValue(value)
+        this.service.getCharacteristic(Characteristic.Active).updateValue(value)
+        this.log('Updated %s to: %s', characteristic, value)
+        break
+      default:
+        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+    }
+  },
+
+  setActive: function (value, callback) {
+    var url = this.apiroute + '/setState/' + value
+    this.log.debug('Setting state: %s', url)
+
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error setting state: %s', error.message)
+        callback(error)
+      } else {
+        this.log('Set state to %s', value)
+        this.service.getCharacteristic(Characteristic.InUse).updateValue(value)
+        callback()
+      }
+    }.bind(this))
+  },
+
+  getServices: function () {
+    this.informationService = new Service.AccessoryInformation()
+    this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, this.model)
+      .setCharacteristic(Characteristic.SerialNumber, this.serial)
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
+
+    this.service.getCharacteristic(Characteristic.ValveType).updateValue(this.valveType)
+
+    this.service
+      .getCharacteristic(Characteristic.Active)
+      .on('set', this.setActive.bind(this))
+
+    this._getStatus(function () {})
+
+    setInterval(function () {
+      this._getStatus(function () {})
+    }.bind(this), this.pollInterval * 1000)
+
+    return [this.informationService, this.service]
+  }
+
+}

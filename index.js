4e354fa2b6da8c7a7f62c5b98438361ac10aec29
phenotypic <33377034+phenotypic@users.noreply.github.com>
2020-05-12 15:40:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,13 +43,12 @@ function WebShower (log, config) {
 
   if (this.listener) {
     this.server = http.createServer(function (request, response) {
-      var parts = request.url.split('/')
-      var partOne = parts[parts.length - 2]
-      var partTwo = parts[parts.length - 1]
-      if (parts.length === 3 && this.requestArray.includes(partOne)) {
-        this.log('Handling request: %s', request.url)
+      var baseURL = 'http://' + request.headers.host + '/'
+      var url = new URL(request.url, baseURL)
+      if (this.requestArray.includes(url.pathname.substr(1))) {
+        this.log.debug('Handling request')
         response.end('Handling request')
-        this._httpHandler(partOne, partTwo)
+        this._httpHandler(url.pathname.substr(1), url.searchParams.get('value'))
       } else {
         this.log.warn('Invalid request: %s', request.url)
         response.end('Invalid request')
@@ -118,7 +117,7 @@ WebShower.prototype = {
   },
 
   setState: function (value, callback) {
-    var url = this.apiroute + '/setState/' + value
+    var url = this.apiroute + '/setState?value=' + value
     // this.log.debug('Setting state: %s', url)
     this.log('Setting state: %s', url)
 
@@ -134,7 +133,7 @@ WebShower.prototype = {
   },
 
   setTemperature: function (value, callback) {
-    var url = this.apiroute + '/setTemperature/' + value
+    var url = this.apiroute + '/setTemperature?value=' + value
     this.log.debug('Setting state: %s', url)
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
@@ -149,7 +148,7 @@ WebShower.prototype = {
   },
 
   setActive: function (head, value, callback) {
-    var url = this.apiroute + '/setState/' + value
+    var url = this.apiroute + '/setState?value=' + value
     this.log.debug('Head %s | Setting state: %s', head, url)
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {

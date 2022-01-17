1a9cd742f947d181a782e4faebcd79ee75fa0681
phenotypic <33377034+phenotypic@users.noreply.github.com>
2022-01-17 23:35:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Service, Characteristic
+let Service, Characteristic
 const packageJson = require('./package.json')
 const request = require('request')
 const ip = require('ip')
@@ -43,12 +43,16 @@ function WebShower (log, config) {
 
   if (this.listener) {
     this.server = http.createServer(function (request, response) {
-      var baseURL = 'http://' + request.headers.host + '/'
-      var url = new URL(request.url, baseURL)
+      const baseURL = 'http://' + request.headers.host + '/'
+      const url = new URL(request.url, baseURL)
       if (this.requestArray.includes(url.pathname.substr(1))) {
-        this.log.debug('Handling request')
-        response.end('Handling request')
-        this._httpHandler(url.pathname.substr(1), url.searchParams.get('value'))
+        try {
+          this.log.debug('Handling request')
+          response.end('Handling request')
+          this._httpHandler(url.pathname.substr(1), url.searchParams.get('value'))
+        } catch (e) {
+          this.log.warn('Error parsing request: %s', e.message)
+        }
       } else {
         this.log.warn('Invalid request: %s', request.url)
         response.end('Invalid request')
@@ -85,7 +89,7 @@ WebShower.prototype = {
   },
 
   _getStatus: function (callback) {
-    var url = this.apiroute + '/status'
+    const url = this.apiroute + '/status'
     this.log.debug('Getting status: %s', url)
 
     this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
@@ -96,7 +100,7 @@ WebShower.prototype = {
       } else {
         this.log.debug('Device response: %s', responseBody)
         try {
-          var json = JSON.parse(responseBody)
+          const json = JSON.parse(responseBody)
           this.service.getCharacteristic(Characteristic.Active).updateValue(json.currentState)
           this.service.getCharacteristic(Characteristic.InUse).updateValue(json.currentState)
           this.log.debug('Updated state to: %s', json.currentState)
@@ -110,18 +114,20 @@ WebShower.prototype = {
 
   _httpHandler: function (characteristic, value) {
     switch (characteristic) {
-      case 'state':
+      case 'state': {
         this.service.getCharacteristic(Characteristic.InUse).updateValue(value)
         this.service.getCharacteristic(Characteristic.Active).updateValue(value)
         this.log('Updated %s to: %s', characteristic, value)
         break
-      default:
+      }
+      default: {
         this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+      }
     }
   },
 
   setState: function (value, callback) {
-    var url = this.apiroute + '/setState?value=' + value
+    const url = this.apiroute + '/setState?value=' + value
     // this.log.debug('Setting state: %s', url)
     this.log('Setting state: %s', url)
 
@@ -137,7 +143,7 @@ WebShower.prototype = {
   },
 
   setTemperature: function (value, callback) {
-    var url = this.apiroute + '/setTemperature?value=' + value
+    const url = this.apiroute + '/setTemperature?value=' + value
     this.log.debug('Setting state: %s', url)
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
@@ -152,7 +158,7 @@ WebShower.prototype = {
   },
 
   setActive: function (head, value, callback) {
-    var url = this.apiroute + '/setState?value=' + value
+    const url = this.apiroute + '/setState?value=' + value
     this.log.debug('Head %s | Setting state: %s', head, url)
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
@@ -185,9 +191,9 @@ WebShower.prototype = {
       .getCharacteristic(Characteristic.Active)
       .on('set', this.setState.bind(this))
 
-    var services = [this.informationService, this.service]
-    for (var head = 1; head <= this.heads; head++) {
-      var accessory = new Service.Valve('Head', head)
+    const services = [this.informationService, this.service]
+    for (let head = 1; head <= this.heads; head++) {
+      const accessory = new Service.Valve('Head', head)
       accessory
         .setCharacteristic(Characteristic.ServiceLabelIndex, head)
         .setCharacteristic(Characteristic.ValveType, 2)
